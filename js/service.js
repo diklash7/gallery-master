7f7e6e33aada206686bf0cf084aa0913107a238e
dikla cohen <diklash7@gmail.com>
2022-02-07 15:47:00 +0200

    profile

---
+++ js/service.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function createProj(name, title) {
+    var proj =
+    {
+        id: makeId(),
+        name: "Minesweeper",
+        title: "Better push those boxes",
+        desc: "lorem ipsum lorem ipsum lorem ipsum",
+        url: "https://diklash7.github.io/final/",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    };
+    return proj;
+
+}
+
